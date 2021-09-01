6e5d58d4de40b69d6b4b1f23e5d746625d81611f
salim-maula <huzaifah956@gmail.com>
2021-09-01 22:20:32 +0700

    Initial commit

---
+++ app/src/main/java/com/salim/mynotesapp/helper/MappingHelper.kt
@@ -0,0 +1,23 @@
+package com.salim.mynotesapp.helper
+
+import android.database.Cursor
+import com.salim.mynotesapp.db.DatabaseContract
+import com.salim.mynotesapp.entity.Note
+
+object MappingHelper {
+
+    fun mapCursorToArrayList(noteCursor: Cursor?): ArrayList<Note>{
+        val notesList = ArrayList<Note>()
+
+        noteCursor?.apply {
+            while (moveToNext()){
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
